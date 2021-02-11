32f0e9b0da2d343cd3200526a00455db3a2f6be7
VtAlmeida <cruiser.vitor@gmail.com>
2021-02-11 11:54:53 -0300

    Deploy

--- script.js
+++ script.js
@@ -14,13 +14,13 @@ let food = {
 
 function criarBG () {
     context.fillStyle = "lightgreen";
-    context.fillRect(0,0,16 * box, 16 * box);
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function criarCobrinha(){
     for (i = 0; i < snake.length; i++){
         context.fillStyle = "green";
-        context.fillRect(snake[i]).x, snake[i].y, box, box);
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
@@ -43,7 +43,7 @@ function iniciarJogo(){
 
     if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if (snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
-    if (snake[0].x > 15 * box && direction == "down") snake[0].y = 0;
+    if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
     for (i = 1; i < snake.length; i++){
